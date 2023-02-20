9842eb564319c5478f44bcd5051986199901d621
joshuachenn <joshuaczh@163.com>
2023-02-20 20:53:22 +0800

    突然提交，很突然

--- SwiftUIGithubApp/Common/Constants.swift
+++ SwiftUIGithubApp/Common/Constants.swift
@@ -11,8 +11,8 @@ let ScreenW = UIScreen.main.bounds.size.width
 let ScreenH = UIScreen.main.bounds.size.height
 
 class NetConfig {
-    static let CLIENT_ID = ""
-    static let CLIENT_SECRET = ""
+    static let CLIENT_ID = "2fd400b86d6c21408c17"
+    static let CLIENT_SECRET = "872ae4de3ba08c199d0bffc5c5fe12f69403eecf"
     
     static let OAuthUrlString = "https://github.com/login/oauth/authorize?client_id=\(NetConfig.CLIENT_ID)&state=app&scope=user,repo,gist,notifications,read:org,workflow&redirect_uri=gsygithubapp://authed"
 }
